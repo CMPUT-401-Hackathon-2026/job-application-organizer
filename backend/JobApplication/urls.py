901c4c4b8330ad4e1529afe590167796f547e66c
Ibrahim Sherwani <isherwan@ualberta.ca>
2026-01-17 15:29:06 -0700

    v2

--- backend/JobApplication/urls.py
+++ backend/JobApplication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import JobApplicationAPIView
 
 urlpatterns = [
-    path("jobs/", JobApplicationAPIView.as_view(), name="jobs"),
+    path("jobs/", JobApplicationAPIView.as_view()),
 ]
